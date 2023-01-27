43de755b76a2c98c0e7c4e375f34984da3b1ca33
Diego Amicabile <diego.amicabile@gmail.com>
2023-01-27 21:47:31 +0100

    split videos in frames mine

--- main.py
+++ main.py
@@ -1,48 +1,53 @@
-import os
-import numpy as np
-import argparse
 import cv2
+import os
+import argparse
+from pathlib import Path
 
-def main():
+def do_split(source, dest_folder):
 
-    # parse all args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('source', type=str, help='Path to source video')
-    parser.add_argument('dest_folder', type=str, help='Path to destination folder')
-    args = parser.parse_args()
+        # get file path for desired video and where to save frames locally
+        cap = cv2.VideoCapture(source)
+        path_to_save = os.path.abspath(dest_folder)
 
-    # get file path for desired video and where to save frames locally
-    cap = cv2.VideoCapture(args.source)
-    path_to_save = os.path.abspath(args.dest_folder)
-    
-    current_frame = 1
+        current_frame = 1
 
-    if (cap.isOpened() == False):
-        print('Cap is not open')
+        if (cap.isOpened() == False):
+            print('Cap is not open')
 
-    finished = False
-    totsec = 0
-    # cap opened successfully
-    while not finished:
-        cap.set(cv2.CAP_PROP_POS_MSEC, totsec * 1000)
-        hasFrames, image = cap.read()
-        # capture each frame
-        ret, frame = cap.read()
-        if (hasFrames):
+        finished = False
+        totsec = 0
+        # cap opened successfully
+        while not finished:
+            cap.set(cv2.CAP_PROP_POS_MSEC, totsec * 1000)
+            hasFrames, image = cap.read()
+            # capture each frame
+            ret, frame = cap.read()
+            if (hasFrames):
 
-            # Save frame as a jpg file
-            name = 'frame' + str(totsec) + '.jpg'
-            cv2.imwrite(os.path.join(path_to_save, name), frame)
+                # Save frame as a jpg file
+                name = 'frame' + str(totsec) + '.jpg'
+                cv2.imwrite(os.path.join(path_to_save, name), frame)
 
-            # keep track of how many images you end up with
-            totsec += 1
-        
-        else:
-            break
+                # keep track of how many images you end up with
+                totsec += 1
 
-    # release capture 
-    cap.release()
-    print('done')
+            else:
+                break
+
+        # release capture
+        cap.release()
+        print('done')
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config')  # option that takes a value
+    args = parser.parse_args()
+    if args.config == None:
+        print("required argument --config <config>")
+    else:
+
+    # parse all args
+
+        source = args.config["source"]
+        dest_folder = args.config["dest_folder"]
+        do_split(source, dest_folder)
