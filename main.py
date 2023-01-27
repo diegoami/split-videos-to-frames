0c04c5605c4e27d2b6813be4ec97ef090efb6253
Diego Amicabile <diego.amicabile@gmail.com>
2023-01-27 22:16:51 +0100

    Split videos into frames

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
 import cv2
 import os
+import yaml
 import argparse
 from pathlib import Path
-
 def do_split(source, dest_folder):
+    source_stem = Path(source).stem
 
-        # get file path for desired video and where to save frames locally
-        cap = cv2.VideoCapture(source)
-        path_to_save = os.path.abspath(dest_folder)
+    # get file path for desired video and where to save frames locally
+    cap = cv2.VideoCapture(source)
+    path_to_save = os.path.abspath(dest_folder)
 
-        current_frame = 1
+    os.makedirs(dest_folder, exist_ok=True)
+    if (cap.isOpened() == False):
+        print('Cap is not open')
 
-        if (cap.isOpened() == False):
-            print('Cap is not open')
+    finished = False
+    totsec = 0
+    # cap opened successfully
+    while not finished:
+        cap.set(cv2.CAP_PROP_POS_MSEC, totsec * 1000)
+        hasFrames, image = cap.read()
+        # capture each frame
+        ret, frame = cap.read()
+        if (hasFrames):
 
-        finished = False
-        totsec = 0
-        # cap opened successfully
-        while not finished:
-            cap.set(cv2.CAP_PROP_POS_MSEC, totsec * 1000)
-            hasFrames, image = cap.read()
-            # capture each frame
-            ret, frame = cap.read()
-            if (hasFrames):
+            # Save frame as a jpg file
+            name = source_stem + '_' + str(totsec) + '.jpg'
+            cv2.imwrite(os.path.join(path_to_save, name), frame)
 
-                # Save frame as a jpg file
-                name = 'frame' + str(totsec) + '.jpg'
-                cv2.imwrite(os.path.join(path_to_save, name), frame)
+            # keep track of how many images you end up with
+            totsec += 1
 
-                # keep track of how many images you end up with
-                totsec += 1
+        else:
+            break
 
-            else:
-                break
+    # release capture
+    cap.release()
+    print('done')
 
-        # release capture
-        cap.release()
-        print('done')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -45,9 +46,8 @@ if __name__ == '__main__':
     if args.config == None:
         print("required argument --config <config>")
     else:
-
-    # parse all args
-
-        source = args.config["source"]
-        dest_folder = args.config["dest_folder"]
-        do_split(source, dest_folder)
+        with open(args.config, 'r') as confhandle:
+            conf_info = yaml.safe_load(confhandle)
+            source = conf_info["source"]
+            dest_folder = conf_info["dest_folder"]
+            do_split(source, dest_folder)
